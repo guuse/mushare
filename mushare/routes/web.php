b13a65cc5dab3f683690cac7362b76afda3a9f03
guuse <ekkelenkampguus@gmail.com>
2016-09-26 14:48:05 +0200

    first view and route
    Database working not implemented

--- mushare/routes/web.php
+++ mushare/routes/web.php
@@ -14,3 +14,11 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/contact', function() {
+    return view('contact');
+});
+
+Route::get('/test', function() {
+    return 'Returning string :)';
+});
